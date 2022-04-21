a28678b0f6db0cdaba924bcdd8ea3afcf032fb16
AH-dark <ahdark@outlook.com>
2022-04-21 23:17:58 +0800

    Feat: test event

--- src/test/java/com/ahdark/code/link/LinkApplicationTests.java
+++ src/test/java/com/ahdark/code/link/LinkApplicationTests.java
@@ -1,8 +1,11 @@
 package com.ahdark.code.link;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class LinkApplicationTests {
 
