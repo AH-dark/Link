2d840c950858d6744605e3bc2f88173cafe96324
AH-dark <ahdark@outlook.com>
2022-04-22 22:24:18 +0800

    Feat: use Session get user info

--- src/main/java/com/ahdark/code/link/controller/api/UserController.java
+++ src/main/java/com/ahdark/code/link/controller/api/UserController.java
@@ -3,9 +3,13 @@ package com.ahdark.code.link.controller.api;
 import com.ahdark.code.link.pojo.User;
 import com.ahdark.code.link.service.UserService;
 import com.ahdark.code.link.utils.ApiResult;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.ahdark.code.link.utils.CodeInfo.USER_ACCOUNT_NOT_EXIST;
+import static com.ahdark.code.link.utils.CodeInfo.USER_NOT_LOGIN;
 
 @RestController
 @RequestMapping(path = "/api/user", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -26,6 +31,34 @@ public class UserController {
     private HttpServletResponse response;
     @Autowired
     private UserService userService;
+    @Autowired
+    private HttpSession session;
+
+    private final Gson gson = new Gson();
+
+    @GetMapping
+    public JSONObject GetByCookie() {
+        String sessionId = session.getId();
+
+        log.info("GET user event, through Cookie.");
+        log.info("Cookie: {}", sessionId);
+
+        Object data =  session.getAttribute(sessionId);
+
+        if (data == null) {
+            log.warn("User not log in, use session id {}", sessionId);
+            return new ApiResult<>(USER_NOT_LOGIN).getJsonResult();
+        }
+
+        log.info("Get session success {}", data);
+        User user = gson.fromJson(gson.toJsonTree(data), User.class);
+
+        ApiResult<User> result = new ApiResult<>(user);
+
+        log.info("Get User success: {}", result);
+
+        return result.getJsonResult();
+    }
 
     @GetMapping(params = {"email"})
     public JSONObject GetByEmail(@RequestParam("email") String email) {
