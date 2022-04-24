7646b6e6be03297b9c9368dcd0a046c43354d7be
AH-dark <ahdark@outlook.com>
2022-04-24 23:05:21 +0800

    Refactor: create sql directory

---
+++ sql/table_config.sql
@@ -0,0 +1,3 @@
+INSERT INTO config (name, value, type) VALUES ('enableTouristShorten', 'true', 'boolean');
+INSERT INTO config (name, value, type) VALUES ('siteName', 'Link', 'string');
+INSERT INTO config (name, value, type) VALUES ('siteUrl', 'https://localhost:8080/', 'string');
